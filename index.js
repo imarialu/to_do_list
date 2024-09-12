ecd1506f48eb7bcfeef9544f33d9d2f2f3e6564d
Maria <marialuizaprado13@gmail.com>
2024-09-12 10:31:23 -0300

    adicionando metas abertas

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let metas = [ meta ]
 const cadastrarMeta = async () => {
     const meta = await input({message: "Digite a meta: "})
 
-    if (meta.length == 0){
+    if(meta.length == 0){
         console.log("A meta não pode ser vazia.")
         return
     }
@@ -32,7 +32,7 @@ const listarMetas = async () => {
         m.checked = false
     })
 
-    if (respostas.length == 0){
+    if(respostas.length == 0){
         console.log("Nenhuma meta foi selecionada!")
         return
     }
@@ -59,8 +59,24 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas realizadas",
+        message: "Metas realizadas " + realizadas.length,
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async() => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true
+    })
+
+    if(abertas.length == 0){
+        console.log("Não existem metas abertas :)")
+        return
+    }
+
+    await select({
+        message: "Metas Abertas: " + abertas.length,
+        choices: [...abertas]
     })
 }
 
@@ -82,6 +98,10 @@ const start = async () => {
                     value: "realizadas"
                 },
                 {
+                    nome: "Metas Abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -98,6 +118,9 @@ const start = async () => {
                 break
             case "realizadas":
                 await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
                 break
             case "sair":
                 console.log("Até mais!")
